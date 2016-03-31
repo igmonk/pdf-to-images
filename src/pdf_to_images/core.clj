3ec4befe482c9791a0d8411584fb62bb585212d4
Igor <Igor@Igors-MacBook-Pro.local>
2016-04-01 01:38:31 +0300

    PDF to images conversion options: pages range (start, end), dpi, resulting image type & extension.

--- src/pdf_to_images/core.clj
+++ src/pdf_to_images/core.clj
@@ -6,22 +6,42 @@
            [org.apache.pdfbox.rendering ImageType]
            [org.apache.pdfbox.tools.imageio ImageIOUtil]))
 
+(defn- range-intersection-for-border-pairs
+  [range-border-pairs]
+  (let [intersection-vec (reduce
+                           (fn [[start1 end1] [start2 end2]]
+                             [(max start1 start2) (min end1 end2)])
+                           (first range-border-pairs)
+                           (rest range-border-pairs))]
+    (range (first intersection-vec)
+           (last intersection-vec))))
+
 (defn pdf-to-images
   [filename & opts]
   (let [pd-document (PDDocument/load (io/file filename))
         pdf-renderer (PDFRenderer. pd-document)
-        pages (vec (.getPages pd-document))]
+        pages (vec (.getPages pd-document))
+        pages-count (count pages)
+        {:keys [start-page end-page dpi ext]
+         :or {start-page 0
+              end-page pages-count
+              dpi 300
+              ext "png"}} opts
+        page-range (range-intersection-for-border-pairs [[0 pages-count]
+                                                         [start-page end-page]])]
     (try
       (doall
-        (map-indexed
-          (fn [index _]
-            (let [image (.renderImageWithDPI pdf-renderer index 100 ImageType/RGB)]
-              (ImageIOUtil/writeImage image (str filename "-" index ".png") 100)))
-          pages))
+        (map
+          (fn [page-index]
+            (let [image (.renderImageWithDPI pdf-renderer page-index dpi ImageType/RGB)
+                  image-filename (str filename "-" page-index "." ext)]
+              (ImageIOUtil/writeImage image image-filename dpi)
+              image-filename))
+          page-range))
       (finally
         (if (not= pd-document nil) (.close pd-document))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (pdf-to-images (first args)))
+  (prn (pdf-to-images (first args) :start-page 0 :end-page 1 :dpi 100 :ext "jpg")))
