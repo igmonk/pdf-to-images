30ea153ef1226663be77b830e051f78e76868941
Igor <Igor@Igors-MacBook-Pro.local>
2016-03-31 23:50:31 +0300

    Close PDDocument object in finally block.

--- src/pdf_to_images/core.clj
+++ src/pdf_to_images/core.clj
@@ -11,16 +11,17 @@
   (let [pd-document (PDDocument/load (io/file filename))
         pdf-renderer (PDFRenderer. pd-document)
         pages (vec (.getPages pd-document))]
-    (doall
-      (map-indexed
-        (fn [index _]
-          (let [image (.renderImageWithDPI pdf-renderer index 100 ImageType/RGB)]
-            (ImageIOUtil/writeImage image (str filename "-" index ".png") 100)))
-        pages))
-    (.close pd-document)))
+    (try
+      (doall
+        (map-indexed
+          (fn [index _]
+            (let [image (.renderImageWithDPI pdf-renderer index 100 ImageType/RGB)]
+              (ImageIOUtil/writeImage image (str filename "-" index ".png") 100)))
+          pages))
+      (finally
+        (if (not= pd-document nil) (.close pd-document))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (str "ARGS:: " args))
   (pdf-to-images (first args)))
