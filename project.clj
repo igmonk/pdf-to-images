5665310c81686fb49f9e8aae1157e78268ae067e
Igor <Igor@Igors-MacBook-Pro.local>
2016-03-31 17:05:13 +0300

    Project description, url. Test success.

--- project.clj
+++ project.clj
@@ -1,9 +1,16 @@
 (defproject pdf-to-images "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+
+  :description "Clojure wrapper for the PDFBox that converts a given page range of a PDF document to bitmap images."
+
+  :url "https://github.com/igor-moiseyenko/pdf-to-images"
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :dependencies [[org.clojure/clojure "1.8.0"]]
+
   :main ^:skip-aot pdf-to-images.core
+
   :target-path "target/%s"
+
   :profiles {:uberjar {:aot :all}})
