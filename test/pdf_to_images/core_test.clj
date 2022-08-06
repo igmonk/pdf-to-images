4b8b125743b5cb8843f9ad04192e75a9c2f03fb9
roboli <robertooliveros@mac.com>
2022-08-06 16:12:19 -0600

    Test image to byte array

--- test/pdf_to_images/core_test.clj
+++ test/pdf_to_images/core_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [clojure.java.io :as io]
             [pdf-to-images.core :refer :all])
-  (:import [javax.imageio ImageIO]
+  (:import [org.apache.pdfbox.tools.imageio ImageIOUtil]
+           [javax.imageio ImageIO]
            (java.io ByteArrayOutputStream)))
 
 (deftest core-all
@@ -16,4 +17,13 @@
       (.flush baos1)
       (ImageIO/write img-2 "png" baos2)
       (.flush baos2)
-      (is (java.util.Arrays/equals (.toByteArray baos1) (.toByteArray baos2))))))
+      (is (java.util.Arrays/equals (.toByteArray baos1) (.toByteArray baos2)))))
+
+  (testing "Image to byte array"
+    (let [path  "test/pdf_to_images/assets/"
+          byrr  (first (pdf-to-images nil image-to-byte-array :pathname (str path "dummy.pdf")))
+          img   (ImageIO/read (io/file (str path "dummy.png")))
+          baos  (ByteArrayOutputStream.)]
+      (ImageIOUtil/writeImage img "png" baos 300)
+      (.flush baos)
+      (is (java.util.Arrays/equals (.toByteArray baos) byrr)))))
