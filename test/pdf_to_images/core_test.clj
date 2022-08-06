7a5b2780bac6961c48cb9fd43d4140954a4d600d
roboli <robertooliveros@mac.com>
2022-08-06 16:01:35 -0600

    Implement file to image test

--- test/pdf_to_images/core_test.clj
+++ test/pdf_to_images/core_test.clj
@@ -1,7 +1,19 @@
 (ns pdf-to-images.core-test
   (:require [clojure.test :refer :all]
-            [pdf-to-images.core :refer :all]))
+            [clojure.java.io :as io]
+            [pdf-to-images.core :refer :all])
+  (:import [javax.imageio ImageIO]
+           (java.io ByteArrayOutputStream)))
 
-(deftest a-test
-  (testing "Success!!!"
-    (is (= 1 1))))
+(deftest core-all
+  (testing "From PDF file to image"
+    (let [path  "test/pdf_to_images/assets/"
+          image (first (pdf-to-images nil image-to-image :pathname (str path "dummy.pdf")))
+          img-2 (ImageIO/read (io/file (str path "dummy.png")))
+          baos1 (ByteArrayOutputStream.)
+          baos2 (ByteArrayOutputStream.)]
+      (ImageIO/write image "png" baos1)
+      (.flush baos1)
+      (ImageIO/write img-2 "png" baos2)
+      (.flush baos2)
+      (is (java.util.Arrays/equals (.toByteArray baos1) (.toByteArray baos2))))))
